a4ea89ed6ea698bbb2cf7c2d1f7198d0819f06e5
Marnix Klooster <marnix.klooster@gmail.com>
2023-12-07 09:14:07 +0100

    Refactoring to make names match tests in book

--- src/canvases.zig
+++ src/canvases.zig
@@ -25,10 +25,14 @@ const Canvas = struct {
         self._allocator.free(self._pixels);
     }
 
-    pub fn at(self: Canvas, x: usize, y: usize) Color {
+    pub fn pixel_at(self: Canvas, x: usize, y: usize) Color {
         return self._pixels[x + y * self._width];
     }
 };
+
+fn canvas(width: usize, height: usize, allocator: *const mem.Allocator) !Canvas {
+    return try Canvas.create(width, height, allocator);
+}
 
 // // // // // // // // // // // // //
 // The following is only for testing
@@ -37,13 +41,13 @@ const testing = @import("testing.zig");
 const expectEqual = testing.expectEqual_;
 
 test "Creating a canvas" {
-    const c: Canvas = try Canvas.create(10, 20, &testing.allocator);
+    const c = try canvas(10, 20, &testing.allocator);
     defer c.deinit();
     try expectEqual(10, c._width);
     try expectEqual(20, c._height);
     for (0..c._width) |x| {
         for (0..c._height) |y| {
-            try expectEqual(color(0, 0, 0), c.at(x, y));
+            try expectEqual(color(0, 0, 0), c.pixel_at(x, y));
         }
     }
 }
