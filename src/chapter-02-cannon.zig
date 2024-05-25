f8f5329b437dc0a9ec510606847211ac2e216e7e
Marnix Klooster <marnix.klooster@gmail.com>
2024-05-25 12:33:44 +0200

    Plot example trajectory from book

--- src/chapter-02-cannon.zig
+++ src/chapter-02-cannon.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 pub const std_options = std.Options{
+    // 'info' is the default log level
     .log_level = .info,
 };
 
@@ -54,8 +55,17 @@ pub fn main() !void {
     const c = try canvas(900, 550, &allocator);
     defer c.deinit();
 
-    const white = color(1, 1, 1);
-    c.write_pixel(450, 225, white);
+    const projectileColor = color(255.0 / 255.0, 165.0 / 255.0, 0.0 / 255.0); // #FFA500 = orange
+
+    var p = Projectile.of(.{ .position = point(0, 1, 0), .velocity = normalize(vector(1, 1.8, 0)).times(11.25) });
+    const e = Environment.of(.{ .gravity = vector(0, -0.1, 0), .wind = vector(-0.01, 0, 0) });
+    var t: usize = 0;
+    while (p.position.y() > 0) {
+        std.log.debug("distance is {}, height is {}", .{ p.position.x(), p.position.y() });
+        c.write_pixel(@intFromFloat(p.position.x()), 550 - @as(usize, @intFromFloat(p.position.y())) - 1, projectileColor);
+        p = tick(e, p);
+        t = t + 1;
+    }
 
     std.log.info("ready to write.", .{});
 
